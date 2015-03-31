c6acac7b69441079b4a62d5611fe0e67d410cd08
santensuru <djuned.ong@gmail.com>
2015-03-31 18:38:32 +0700

    beranda :v belum bisa navigasi

--- bahari/MainWindow.xaml.cs
+++ bahari/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using FirstFloor.ModernUI.Windows.Controls;
+using FirstFloor.ModernUI.Windows.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,44 @@ namespace bahari
         public MainWindow()
         {
             InitializeComponent();
+            this.Title = "Bahari - Main Home :)";
+        }
+
+        private void Map_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("Map", ":)", MessageBoxButton.OK);
+        }
+
+        private void AboutUs_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("About Us", ":)", MessageBoxButton.OK);
+            //NavigationService nav = NavigationService.GetNavigationService(this);
+            //nav.Navigate(new Uri("AboutUs.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void Galery_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("Gallery", ":)", MessageBoxButton.OK);
+        }
+
+        private void Appendix_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("Appendix", ":)", MessageBoxButton.OK);
+        }
+
+        private void Games_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("Games", ":)", MessageBoxButton.OK);
+        }
+
+        private void Me_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("Me", ":)", MessageBoxButton.OK);
+        }
+
+        private void Information_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ModernDialog.ShowMessage("Information", ":)", MessageBoxButton.OK);
         }
     }
 }
